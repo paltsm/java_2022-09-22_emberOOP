d9e16cfc5387435e30dfdebbb6a034a86d4f4a79
misi <misipalotas@gmail.com>
2022-09-22 16:07:02 +0200

    nem ember osztályt csináltam :trollface:

--- src/hu/petrik/emberOOP/Main.java
+++ src/hu/petrik/emberOOP/Main.java
@@ -2,6 +2,12 @@ package hu.petrik.emberOOP;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Ember e1=new Ember("Gipsz Jakab","1988-1-12","Budapest");
+		Ember e2=new Ember("Teszt Elek","2003-12-5","Debrecen");
+		Ember e3=new Ember("Kovács István","1973-9-22","Pécs");
+
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
 	}
 }
